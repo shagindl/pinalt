7c2e7512eda424aa8f8b70e34f7f983da350d0a6
Шагин Д.Л <dlsh.nn@gmail.com>
2021-01-14 16:26:03 +0300

    Реализовал механизм преобразования AD2Q. Необходимо проверка в проекте Quartus.

--- pinalt.py
+++ pinalt.py
@@ -4,6 +4,7 @@ import sys
 import csv
 import os
 
+VERSION = '0.0.1'
 # ------------------------------------------------------------------------------
 #
 # ------------------------------------------------------------------------------
@@ -80,6 +81,38 @@ def pins_extraction(filename, exclude_nets):
     fdwt.write(netcontent)
     fdwt.close()
 
+def AD2Q(pinAD, association_name, filename):
+    content = ''
+    content = content + '\n\t# pinalt(ium).AD2Q generator version: %s\n\n' % VERSION
+    # Head
+    content += 'To,Assignment Name,Value\n'
+    for i, Npin in enumerate(pinAD._dict['Pin Designator']):
+        if pinAD._dict['Net Name'][i] in association_name._dict['Net Name']:
+           j = next( j for j, comp in enumerate(association_name._dict['Net Name']) if comp == pinAD._dict['Net Name'][i])
+           content += '%s,Location,PIN_%s\n' % (association_name._dict['To'][j], Npin)
+    # Get pure filename
+    os_filename = os.path.basename(filename)
+    index_of_dot = os_filename.index('.')
+    pure_filename = os_filename[:index_of_dot]
+
+    # Saving to files
+    fdwt = open((pure_filename + '.csv'), "w")
+    fdwt.write(content)
+    fdwt.close()
+class scv2list:
+    def __init__(self, filename):
+        self._dict = {}
+        fHeader = True
+        with open(filename, "r") as pf:
+            reader = csv.reader(pf, delimiter=',')
+            for row in reader:
+                if len(row) <= 1: continue
+                if fHeader:
+                    fHeader = False
+                    self._dict = { name : [] for name in row}
+                else:
+                    for i, key in enumerate(self._dict.keys()):
+                        self._dict[key].append(row[i])
 # ------------------------------------------------------------------------------
 #
 # ------------------------------------------------------------------------------
@@ -96,11 +129,20 @@ if __name__ == '__main__':
 
     exclude_nets = []
 
-    if nargs < 1:
-        error("ERROR: %d arguments less then 1;" % nargs)
+    if nargs < 4:
+        error("ERROR: %d arguments less then 3;" % nargs)
     else:
+        key = sys.argv[1]
+        if key == "-AD2Q":
+            pinAD = scv2list(sys.argv[2])
+            association_name = scv2list(sys.argv[3])
+
+            AD2Q(pinAD, association_name, sys.argv[4])
+        else:
+            print('Not defined Key = ', key, '. Key = -AD2Q')
+
         for net in sys.argv[2:]:
-            exclude_nets.append(net.lower());
+            exclude_nets.append(net.lower())
         try:
             pins_extraction(sys.argv[1], exclude_nets)
         except IOError:
